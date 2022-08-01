5888aaf7816f8ea99b5ec26a645a6c43c958f35c
Raunak0604 <raunakmondal0604@gmail.com>
2022-08-01 15:37:16 +0530

    file added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://rabshare.herokuapp.com";
+const baseURL = "https://rabshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
